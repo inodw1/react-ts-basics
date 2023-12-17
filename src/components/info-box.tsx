4494b909e3310a892836a95ae56f580069525d9f
inodw1 <inodwagachchi@gmail.com>
2023-12-17 10:10:52 +0530

    Combine discriminated props

--- src/components/info-box.tsx
+++ src/components/info-box.tsx
@@ -1,12 +1,20 @@
 import React, { type ReactNode } from "react";
 
-interface InfoBoxProps {
-    mode: "hint" | "warning";
+interface HintBoxProps {
+    mode: "hint";
+    children: ReactNode;
+}
+
+interface warningBoxProps {
+    mode: "warning";
     severity?: "low" | "medium" | "high";
     children: ReactNode;
 }
 
-const InfoBox = ({ mode, children, severity }: InfoBoxProps) => {
+type InfoBoxProps = HintBoxProps | warningBoxProps;
+
+const InfoBox = (props: InfoBoxProps) => {
+    const { mode, children } = props;
     if (mode === "hint") {
         return (
             <aside className="infobox infobox-hint">
@@ -14,6 +22,7 @@ const InfoBox = ({ mode, children, severity }: InfoBoxProps) => {
             </aside>
         );
     }
+    const { severity } = props;
     return (
         <aside className={`infobox infobox-warning warning--${severity}`}>
             <h2>Warning</h2>

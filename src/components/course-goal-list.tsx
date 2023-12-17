f881910291af74dbf1f8c8ea6d4a783906760a64
inodw1 <inodwagachchi@gmail.com>
2023-12-17 09:41:13 +0530

    Build dynamic info box component

--- src/components/course-goal-list.tsx
+++ src/components/course-goal-list.tsx
@@ -1,6 +1,7 @@
-import React, { type FC } from "react";
+import React, { type ReactNode, type FC } from "react";
 import { type CourseGoal as CourseGoalObj } from "../App";
 import CourseGoal from "./course-goal";
+import InfoBox from "./info-box";
 
 interface CourseGoalListProps {
     goals: CourseGoalObj[]; // Array<CourseGoalObj>
@@ -8,20 +9,41 @@ interface CourseGoalListProps {
 }
 
 const CourseGoalList: FC<CourseGoalListProps> = ({ goals, onDeleteGoal }) => {
+    if (goals.length === 0) {
+        return (
+            <InfoBox mode="hint">
+                You have no course goals yet. Start adding some!
+            </InfoBox>
+        );
+    }
+
+    let warningBox: ReactNode;
+    if (goals.length >= 4) {
+        warningBox = (
+            <InfoBox mode="warning">
+                You're collecting a lot of goals. Don't put too much on your
+                plate.
+            </InfoBox>
+        );
+    }
+
     return (
-        <ul>
-            {goals.map((goal) => (
-                <li key={goal.id}>
-                    <CourseGoal
-                        id={goal.id}
-                        title={goal.title}
-                        onDelete={onDeleteGoal}
-                    >
-                        <p>{goal.description}</p>
-                    </CourseGoal>
-                </li>
-            ))}
-        </ul>
+        <>
+            {warningBox}
+            <ul>
+                {goals.map((goal) => (
+                    <li key={goal.id}>
+                        <CourseGoal
+                            id={goal.id}
+                            title={goal.title}
+                            onDelete={onDeleteGoal}
+                        >
+                            <p>{goal.description}</p>
+                        </CourseGoal>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 };
 

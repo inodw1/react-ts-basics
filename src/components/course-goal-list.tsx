3373c4ecd6a62cf5dffcc7dcd07016979a8be61a
inodw1 <inodwagachchi@gmail.com>
2023-12-17 10:00:30 +0530

    Handle warning level according to the severity prop

--- src/components/course-goal-list.tsx
+++ src/components/course-goal-list.tsx
@@ -20,7 +20,7 @@ const CourseGoalList: FC<CourseGoalListProps> = ({ goals, onDeleteGoal }) => {
     let warningBox: ReactNode;
     if (goals.length >= 4) {
         warningBox = (
-            <InfoBox mode="warning">
+            <InfoBox mode="warning" severity="medium">
                 You're collecting a lot of goals. Don't put too much on your
                 plate.
             </InfoBox>
--- src/components/info-box.tsx
+++ src/components/info-box.tsx
@@ -2,10 +2,11 @@ import React, { type ReactNode } from "react";
 
 interface InfoBoxProps {
     mode: "hint" | "warning";
+    severity?: "low" | "medium" | "high";
     children: ReactNode;
 }
 
-const InfoBox = ({ mode, children }: InfoBoxProps) => {
+const InfoBox = ({ mode, children, severity }: InfoBoxProps) => {
     if (mode === "hint") {
         return (
             <aside className="infobox infobox-hint">
@@ -14,7 +15,7 @@ const InfoBox = ({ mode, children }: InfoBoxProps) => {
         );
     }
     return (
-        <aside className="infobox infobox-warning warning--medium">
+        <aside className={`infobox infobox-warning warning--${severity}`}>
             <h2>Warning</h2>
             <p>{children}</p>
         </aside>
